2de1f7e3afb697d36098e8647f0d00f2b0dd9dd9
haraya_kouhei <haraya_kouhei@5star.co.jp>
2026-09-04 16:43:51 +0900

    野球アプリのパスワード変更

--- src/data/studies.ts
+++ src/data/studies.ts
@@ -8,7 +8,7 @@ export const studiesData: Study[] = [
     slug: "1",
     title: "野球観戦記録アプリ",
     longDescription:
-      "【デモ用アカウント】\nユーザー名：baseball-app\nパスワード：auZPA9cF\n\nプロ野球ファン(自分)のための、データ連動型観戦記録Webアプリケーションです。単なるメモ帳ではなく、公式サイト（npb.jp）から試合結果を自動取得し、自身の観戦データと紐付けることで、応援球団の観戦勝率などをリアルタイムに可視化します。next.js・react・TypeScriptなどモダンな技術の勉強をするためにClaude CodeというAIを使用しながら開発しました。バックエンドにはsupabaseを採用しております。",
+      "【デモ用アカウント】\nユーザー名：baseball_user\nパスワード：Awiuq&5lKVyr\n\nプロ野球ファン(自分)のための、データ連動型観戦記録Webアプリケーションです。単なるメモ帳ではなく、公式サイト（npb.jp）から試合結果を自動取得し、自身の観戦データと紐付けることで、応援球団の観戦勝率などをリアルタイムに可視化します。next.js・react・TypeScriptなどモダンな技術の勉強をするためにClaude CodeというAIを使用しながら開発しました。バックエンドにはsupabaseを採用しております。",
     image: "/img/study-img01.jpg",
     role: "next.js / react",
     externalLink: "https://baseball-app-mu.vercel.app/",
